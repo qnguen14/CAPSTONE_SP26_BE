1a935f6df51e122e6713f636007e9fafc9ddb672
Moi2k4 <manh310104@gmail.com>
2026-01-14 14:21:13 +0700

    ADD: worker, farmer profile entities

---
+++ AgroTemp.Domain/Entities/Farm.cs
@@ -0,0 +1,48 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AgroTemp.Domain.Entities;
+
+[Table("Farm")]
+public class Farm
+{
+    [Key]
+    [Required]
+    [Column("id")]
+    public Guid Id { get; set; }
+
+    [Required]
+    [ForeignKey(nameof(FarmerProfile))]
+    [Column("farmer_profile_id")]
+    public Guid FarmerProfileId { get; set; }
+    public virtual FarmerProfile FarmerProfile { get; set; }
+
+    [Required]
+    [Column("address")]
+    public string Address { get; set; }
+
+    [Required]
+    [Column("latitude")]
+    public decimal Latitude { get; set; }
+
+    [Required]
+    [Column("longitude")]
+    public decimal Longitude { get; set; }
+
+    [Required]
+    [Column("location_name")]
+    [StringLength(256)]
+    public string LocationName { get; set; }
+
+    [Required]
+    [Column("is_primary")]
+    public bool IsPrimary { get; set; }
+
+    [Required]
+    [Column("created_at")]
+    public DateTime CreatedAt { get; set; }
+
+    [Required]
+    [Column("updated_at")]
+    public DateTime UpdatedAt { get; set; }
+}
---
+++ AgroTemp.Domain/Entities/WorkerProfile.cs
@@ -0,0 +1,72 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AgroTemp.Domain.Entities;
+
+public enum ExperienceLevel
+{
+    Beginner = 1,
+    Intermediate = 2,
+    Experienced = 3
+}
+
+[Table("Worker_Profile")]
+public class WorkerProfile
+{
+    [Key]
+    [Required]
+    [Column("id")]
+    public Guid Id { get; set; }
+
+    [Required]
+    [ForeignKey(nameof(User))]
+    [Column("user_id")]
+    public Guid UserId { get; set; }
+    public virtual User User { get; set; }
+
+    [Required]
+    [Column("full_name")]
+    [StringLength(256)]
+    public string FullName { get; set; }
+
+    [Required]
+    [Column("age_range")]
+    [StringLength(50)]
+    public string AgeRange { get; set; }
+
+    [Required]
+    [Column("primary_location")]
+    public string PrimaryLocation { get; set; }
+
+    [Column("travel_radius_km_preference")]
+    public double? TravelRadiusKmPreference { get; set; }
+
+    [Required]
+    [Column("experience_level")]
+    public int ExperienceLevelId { get; set; }
+    public virtual ExperienceLevel ExperienceLevel { get; set; }
+
+    [Required]
+    [Column("average_rating")]
+    public decimal AverageRating { get; set; }
+
+    [Required]
+    [Column("availability_schedule")]
+    public string AvailabilitySchedule { get; set; }
+
+    [Required]
+    [Column("total_jobs_completed")]
+    public int TotalJobsCompleted { get; set; }
+
+    [Required]
+    [Column("avatar_url")]
+    public string AvatarUrl { get; set; }
+
+    [Required]
+    [Column("created_at")]
+    public DateTime CreatedAt { get; set; }
+
+    [Required]
+    [Column("updated_at")]
+    public DateTime UpdatedAt { get; set; }
+}
